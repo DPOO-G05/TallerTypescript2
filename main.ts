73b7c63bcddf1db1394a08701315e68381572a8c
EstebanLeiva <e.leivam@uniandes.edu.co>
2022-10-23 23:05:34 -0500

    Terminado

--- main.ts
+++ main.ts
@@ -8,10 +8,11 @@ function insertSeries(series: Serie[]): void {
     series.forEach(s => {
         let tr: HTMLElement = document.createElement('tr');
         tr.innerHTML = `  <td class="table-active"> ${s.id} </td>
-                          <td class="table-active"><a href=${s.link} target="_blank"> ${s.name} </td>
+                          <td class="table-active"><a href="#" style="text-decoration: none;"> ${s.name} </td>
                           <td class="table-active"> ${s.channel} </td>
                           <td class="table-active"> ${s.seasons} </td>`;
         tbody.appendChild(tr);
+        tr.addEventListener("click", () => insertCardDetail(s))   
     });
 }
 
@@ -26,6 +27,25 @@ function insertSeasonAverage(series: Serie[]): void {
 
 }
 
+const cardTbody: HTMLElement = document.getElementById("card")!
+
+function insertCardDetail(serie: Serie): void {
+    console.log('Insertando carta.');
+    cardTbody.innerHTML=`
+    <div class="card mx-auto mx-lg-1" style="width: 18rem;">
+      <img src="${serie.cover}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name} </h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.link}">${serie.link}</a>
+      </div>
+    </div>`
+
+    //cardTbody.appendChild(cardElement)
+
+
+}
 
 insertSeries(series);
 insertSeasonAverage(series);
+
